eff407cbae7efd9e147ffe81b589dd2c4158c951
Vadim <v.baranovski@invatechs.com>
2021-09-17 13:07:25 +0300

    init

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import * as express from 'express';
+
+const app = express();
+
+const aa = '120max 120max 120max 120max 120max 120max 120max 120max 120max 120max 120max 120max 120max 120max 120max 120max';
+
+app.listen(3000, () => console.log('started'));
